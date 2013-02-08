db0b8518c7709cb218131fa653c9f4aa020aa9c3
HAYASI Hideki <linxs@linxs.org>
2013-02-08 15:51:01 +0900

    Other Changes - Added setup.py. - Added dsview.py, a sample program. - Added docushare/properties.xls for convenience in modifications on CSV.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2.6
+# vim: fileencoding=utf-8
+
+from distutils.core import setup
+
+from docushare.__init__ import \
+        __author__, __copyright__, __license__, __version__, __email__
+
+
+setup(
+    name="docushare",
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    url="http://launchpad.net/docushare",
+    description="A DocuShare Client Library.",
+    long_description="""`docushare' is a client library for DocuShare by Xerox Corporation.""",
+    license=__license__,
+    platforms=["win32",],
+    packages=["docushare",],
+    package_data={"docushare": ["*.csv",]},
+    zipfile="docushare.zip",
+    )
